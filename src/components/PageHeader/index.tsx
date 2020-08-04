48b2e1ed590f53bcf8ac6ad1d7b629c05a5d3091
arthur-andraade <arthurandradenf@gmail.com>
2020-08-04 12:51:36 -0300

    Update and same changes

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -22,6 +22,7 @@ const PageHeader: React.FunctionComponent<PageHeaderProps> = (props) => {
 
             <div className="header-content">
                 <strong>{props.title}</strong>
+                {props.children}
             </div>
         </header>
     )
