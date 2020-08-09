10ec486d70bd8117b0cc831a46346a880e722497
arthur-andraade <arthurandradenf@gmail.com>
2020-08-09 17:26:13 -0300

    Creating tabs to screens

---
+++ mobile/src/pages/Favorites/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import styles from './style';
+
+function Favorites() {
+    return <View />
+}
+
+export default Favorites;
---
+++ mobile/src/pages/TeacherList/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import styles from './style';
+
+function TeacherList() {
+    return <View />
+}
+
+export default TeacherList;
