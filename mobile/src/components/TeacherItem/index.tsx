70c725c14c7e4be30e1816bb59d6672fef63d1a3
arthur-andraade <arthurandradenf@gmail.com>
2020-08-11 21:18:51 -0300

    Adding system the favorites of teachers

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import { View, Image, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Image, Text, Linking } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import styles from './style';
-import { RectButton } from 'react-native-gesture-handler';
 
 import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
 import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
 import whatsappIcon from '../../assets/images/icons/whatsapp.png';
+import api from '../../services/api';
 
 export interface Teacher {
     id: number,
@@ -19,10 +21,50 @@ export interface Teacher {
 }
 
 interface TeacherItemProps {
-    teacher: Teacher
+    teacher: Teacher,
+    favorited: boolean;
 }
 
-const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher, favorited }) => {
+
+    const [isFavorited, setIsFavorited] = useState(favorited);
+
+    function handleLink() {
+        api.post('connections', {
+            user_id: teacher.id
+        });
+
+        Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`)
+    }
+
+    async function handleToggleFavorite() {
+
+        const favorites = await AsyncStorage.getItem('favorites');
+
+        let favoritesArray = [];
+
+        if (favorites) {
+            favoritesArray = JSON.parse(favorites);
+        }
+
+        if (isFavorited) {
+            const favoriteIndex = favoritesArray.findIndex(
+                (teacherItem: Teacher) => {
+                    return teacherItem.id === teacher.id;
+                }
+            );
+
+            favoritesArray.splice(favoriteIndex, 1);
+            setIsFavorited(false);
+        } else {
+
+            favoritesArray.push(teacher);
+            setIsFavorited(true);
+        }
+
+        await AsyncStorage.setItem('favorites', JSON.stringify(favoritesArray));
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.profile}>
@@ -46,11 +88,20 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
                 </Text>
 
                 <View style={styles.buttonsContainer}>
-                    <RectButton style={[styles.favoriteButton, styles.favorited]}>
-                        <Image source={heartOutlineIcon} />
+                    <RectButton
+                        onPress={handleToggleFavorite}
+                        style={[
+                            styles.favoriteButton,
+                            isFavorited ? styles.favorited : {}
+                        ]}
+                    >
+                        {isFavorited
+                            ? <Image source={unfavoriteIcon} />
+                            : <Image source={heartOutlineIcon} />
+                        }
                     </RectButton>
 
-                    <RectButton style={styles.contactButton}>
+                    <RectButton style={styles.contactButton} onPress={handleLink}>
                         <Image source={whatsappIcon} />
                         <Text style={styles.contactButtonText}>Entrar em contato</Text>
                     </RectButton>
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,27 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, ScrollView, Text, TextInput } from 'react-native';
 import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
-
+import AsyncStorage from '@react-native-community/async-storage';
 import styles from './style';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem, { Teacher } from '../../components/TeacherItem';
 import api from '../../services/api';
+import { useFocusEffect } from '@react-navigation/native';
 
 function TeacherList() {
 
     const [teachers, setTeachers] = useState([]);
+    const [favorites, setFavorites] = useState<number[]>([]);
     const [isFilterVisible, setIsFiltersVisible] = useState(false);
 
     const [subject, setSubject] = useState('');
     const [week_day, setWeekDay] = useState('');
     const [time, setTime] = useState('');
 
+    useFocusEffect(() => {
+        loadFavorites();
+    })
+
+
+    function loadFavorites() {
+        AsyncStorage.getItem('favorites').then(response => {
+            if (response) {
+
+                const favoritedTeachers = JSON.parse(response);
+                const favoritedTeachersId = favoritedTeachers.map(
+                    (teacher: Teacher) => { return teacher.id }
+                );
+
+                setFavorites(favoritedTeachersId);
+            }
+        })
+    }
+
     function handleToggleFiltersVisible() {
         setIsFiltersVisible(!isFilterVisible);
     }
 
     async function handleFiltersSubmit() {
+        loadFavorites();
         const response = await api.get('classes', {
             params: {
                 subject,
@@ -93,7 +115,12 @@ function TeacherList() {
                 }}
             >
                 {teachers.map((teacher: Teacher) => {
-                    return <TeacherItem key={teacher.id} teacher={teacher} />
+                    return (
+                        <TeacherItem
+                            key={teacher.id}
+                            teacher={teacher}
+                            favorited={favorites.includes(teacher.id)}
+                        />)
                 })}
             </ScrollView>
         </View>
